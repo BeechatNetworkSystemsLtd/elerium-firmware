9deacea0abd4e0f49673d8385991115fb1053b17
Maksym Ostapenko <maksym.ostapenko@embrox.com>
2024-09-09 19:56:23 +0300

    fix: clock source for rng peripheral

--- boards/beechat/elerium/board.cmake
+++ boards/beechat/elerium/board.cmake
@@ -1,5 +1,7 @@
 
 board_runner_args(jlink "--device=STM32L412K8" "--speed=4000")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
