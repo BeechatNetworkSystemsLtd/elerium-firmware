008ba47f8f3417da0f71f972e1ebac303f129507
Maksym Ostapenko <maksym.ostapenko@embrox.com>
2024-09-16 17:51:14 +0300

    fix: entropy for ecc generation

--- lib/subsys/crypto_tinycrypt.c
+++ lib/subsys/crypto_tinycrypt.c
@@ -27,11 +27,12 @@ struct {
 //***************************************************************************//
 
 int default_CSPRNG(uint8_t* dest, unsigned int size) {
-    int res = tc_ctr_prng_generate(&mod.prng, NULL, 0, dest, size);
-    return res;
+    sys_rand_get(dest, size);
+    return TC_CRYPTO_SUCCESS;
 }
 
 int elerium_crypto_generate(struct elerium_priv_key* priv_key, struct elerium_pub_key* pub_key) {
+    uECC_set_rng(&default_CSPRNG);
     const int rc = uECC_make_key(pub_key->data, priv_key->data, uECC_secp256r1());
     return (rc == TC_CRYPTO_SUCCESS) ? 0 : -EFAULT;
 }
@@ -50,7 +51,6 @@ int elerium_crypto_sign(struct elerium_priv_key* priv_key,
     return (rc == TC_CRYPTO_SUCCESS) ? 0 : -EFAULT;
 }
 
-
 int elerium_crypto_verify(struct elerium_pub_key* pub_key,
                           const void* hash,
                           size_t hash_len,
@@ -64,7 +64,6 @@ int elerium_crypto_verify(struct elerium_pub_key* pub_key,
 
     return (rc == TC_CRYPTO_SUCCESS) ? 0 : -EFAULT;
 }
-
 
 int elerium_crypto_sha256(const void* data, size_t data_len, struct elerium_hash* hash) {
     int rc = 0;
@@ -111,8 +110,6 @@ int crypto_init(void) {
     if (rc == TC_CRYPTO_SUCCESS) {
         rc = 0;
     }
-
-    uECC_set_rng(&default_CSPRNG);
 
     return rc;
 }
